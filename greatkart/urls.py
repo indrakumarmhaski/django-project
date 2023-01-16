7b8de11f8b6cb6d0840301eb1099dbc0731afd5b
indrakumarmhaski <indrakumarm@iitbhilai.ac.in>
2023-01-16 19:45:54 +0530

    django authentication

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -1,5 +1,4 @@
 """greatkart URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -15,13 +14,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views, settings
+from . import views
 from django.conf.urls.static import static
-
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
